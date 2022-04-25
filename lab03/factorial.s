da0a963ba720667fc92f3a17d58052dd62057573
moonriver0922 <3190102029@zju.edu.cn>
2022-04-25 15:42:41 +0800

    finished lab03

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -22,3 +22,14 @@ main:
 
 factorial:
     # YOUR CODE HERE
+    lw a2, 0(t0)
+    addi a3, x0, 1
+    beq a2, a3, exit # if a2 == x0 then exit
+    addi a1, a2, -1
+    mul a0, a0, a1
+    addi a2, a2, -1
+    sw a2, 0(t0)
+    j factorial  # jump to factorial
+    
+exit:
+    jr ra   # jump to ra
