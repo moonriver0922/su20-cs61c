44dbe69f97d4e464e40d91f17b40c55a831cbb36
moonriver0922 <3190102029@zju.edu.cn>
2022-05-12 14:09:01 +0800

    finished lab04

--- lab04/discrete_fn.s
+++ lab04/discrete_fn.s
@@ -77,7 +77,11 @@ main:
 # Think: why might having a1 be useful?
 f:
     # YOUR CODE GOES HERE!
-
+    addi t0 a0 3
+    li t1 4
+    mul t1 t1 t0
+    add a1 a1 t1
+    lw a0 0(a1)
     jr ra               # Always remember to jr ra after your function!
 
 print_int:
